84dbc2b38c7a91befdde8649633d6d39cd231616
Alexander Serebryakov <alex.serebr@gmail.com>
2021-01-09 00:22:46 +0200

    Read list of stations in state

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+extern crate reqwest;
+extern crate json;
+
+#[tokio::main]
+
+async fn main() -> Result<(), reqwest::Error> {
+    let res = reqwest::get("https://eddbapi.kodeblox.com/api/v4/stations?economyname=Refinery&statenames=Infrastructure Failure").await?;
+
+    println!("Status: {}", res.status());
+
+    let body = res.text().await?;
+
+    println!("Body:\n\n{}", body);
+
+    let parsed = json::parse(&body).unwrap();
+
+    Ok(())
 }
+
+//fn main() {}
