de50740c7f6a7fe28a0e559ccb27bff9aae3ca18
Alexander Serebryakov <alex.serebr@gmail.com>
2021-01-09 20:10:32 +0200

    And now it is pretty printed

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,14 @@
 extern crate reqwest;
 extern crate json;
 
+struct CommodityData {
+    system: String,
+    station: String,
+    name: String,
+    supply: u32,
+    price: u32,
+}
+
 async fn get_page(number: u32) -> Result<String, reqwest::Error> {
     let request = format!("https://eddbapi.kodeblox.com/api/v4/stations?economyname=Refinery&statenames=Infrastructure Failure&page={}", number);
     reqwest::get(&request).await?.text().await
@@ -13,12 +21,20 @@ async fn get_market(id: String) -> Result<String, reqwest::Error> {
     reqwest::get(&request).await?.text().await
 }
 
-fn print_silver_data(market: json::JsonValue) {
+fn get_commodity_data(market: &json::JsonValue, commodity_name: String) -> Option<CommodityData> {
     for commodity in market["commodities"].members() {
-        if (commodity["name"].to_string() == "Silver") {
-            println!("system {:20} station {:20} {} price {:20} stock {:20}", market["name"], market["sName"], commodity["name"], commodity["sellPrice"], commodity["stock"]);
+        if commodity["name"].to_string() == commodity_name {
+            return Some(CommodityData {
+                system: market["name"].to_string(),
+                station: market["sName"].to_string(),
+                name: commodity_name,
+                supply: commodity["stock"].as_u32().unwrap(),
+                price: commodity["sellPrice"].as_u32().unwrap(),
+            })
         }
     }
+
+    None
 }
 
 #[tokio::main]
@@ -28,13 +44,20 @@ async fn main() -> Result<(), reqwest::Error> {
     let number_of_pages = parsed["pages"].as_u32().unwrap();
     println!("pages {}", number_of_pages);
 
-    for page in 2..number_of_pages {
+    for page in 1..number_of_pages {
         let body = get_page(page).await?;
         let parsed = json::parse(&body).unwrap();
         for station in parsed["docs"].members() {
             let market = get_market(station["ed_market_id"].to_string()).await?;
             let market_json = json::parse(&market).unwrap();
-            print_silver_data(market_json);
+            if let Some(commodity_data) = get_commodity_data(&market_json, String::from("Silver")) {
+            println!("{:20} Station {:30} {} Price {:10} Supply {:10}",
+                commodity_data.system,
+                commodity_data.station,
+                commodity_data.name,
+                commodity_data.price,
+                commodity_data.supply);
+            }
         }
     }
 
