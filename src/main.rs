0d7aa21ee831f2a3718002d9b293fe8e72b33c03
Alexander Serebryakov <alex.serebr@gmail.com>
2021-01-09 13:18:03 +0200

    Now it can print silver market price

--- src/main.rs
+++ src/main.rs
@@ -13,17 +13,27 @@ async fn get_market(id: String) -> Result<String, reqwest::Error> {
     reqwest::get(&request).await?.text().await
 }
 
+fn print_silver_data(market: json::JsonValue) {
+    for commodity in market["commodities"].members() {
+        if (commodity["name"].to_string() == "Silver") {
+            println!("system {} station {}", market["name"].to_string(), market["sName"].to_string());
+            println!("{} price {} stock {}", commodity["name"].to_string(), commodity["sellPrice"], commodity["demand"]);
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
     let body = get_page(1).await?;
 
     let parsed = json::parse(&body).unwrap();
-    
+
     for station in parsed["docs"].members() {
-        println!("id = {}", station["ed_market_id"].to_string());
+        // println!("id = {}", station["ed_market_id"].to_string());
         let market = get_market(station["ed_market_id"].to_string()).await?;
         let market_json = json::parse(&market).unwrap();
-        println!("{}", market_json.pretty(4));
+        print_silver_data(market_json);
+        //println!("{}", market_json.pretty(4));
     }
 
     Ok(())
